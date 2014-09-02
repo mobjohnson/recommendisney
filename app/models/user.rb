fda4df89cce25cac8c751916775d8e8c8ce82586
mobjohnson <mobjohnson@gmail.com>
2014-09-02 17:29:51 -0400

    Move logic to Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  
+  def food_recommendations
+  	if self.age > 20 
+  		Restaurant.all
+  	else
+  		Restaurant.where(:serves_alcohol => false)
+  	end
+  end
 end
